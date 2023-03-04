f1b2d90269bea18dd3a1bd8e8f549af7e81b3d5e
Ashish Zapadiya <sirzapadiya@gmail.com>
2023-03-04 11:39:15 +0530

    Practice Problems

---
+++ Practice Problems/percentile_score.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class percentile_score{
+
+    public static void main(String args[]) {
+        
+        // Scanner sc = new Scanner(System.in);
+
+        float t = 7;
+        int arr[] ={800, 300, 950, 760, 680, 490, 640};
+        int s = 760;
+        float cnt=0; 
+        
+        for(int i=0; i<t; i++){
+            if(arr[i]<s){
+                cnt++;
+            }
+        }
+
+        float p = (cnt / t) * 100;
+        System.out.printf("%.2f",p);
+        
+    }
+}
